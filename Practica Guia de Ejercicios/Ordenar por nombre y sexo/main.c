835abd2ad0519f85a78bbf9df8cd774b88382242
danielclas <danielclas@outlook.es>
2019-04-06 16:21:43 -0300

    Practica

--- Practica Guia de Ejercicios/Ordenar por nombre y sexo/main.c
+++ Practica Guia de Ejercicios/Ordenar por nombre y sexo/main.c
@@ -5,36 +5,46 @@
 /*
 Ejercicio para practicar funcion que ordene primero por nombre, y despues por sexo y nombre
 */
-void ordenarPorNombre (char nombre[][20], int tam);
+void ordenarPorNombre (char nombre[][20], char sexos[], int tam);
+void ordenarPorNombreYSexo (char nombre[][20], char sexos[], int tam);
+
 
 
 int main()
 {
 
-    char nombre [][20]={"Juan Perez", "Maria Belen", "Daniel Clas", "Pedro Alberto", "Lucas Lopez"};
+    char nombre [][20]={"Juan Perez", "Maria Belen", "Daniel Clas", "Camila Alvarez", "Lucas Lopez"};
+    char sexos [5]={'m','f','m','f','m'};
 
     for(int i=0 ; i<5 ; i++){
-        puts(nombre[i]);
-        printf("\n");
+        printf("\n%s, %c", nombre[i], sexos[i]);
     }
 
-    ordenarPorNombre(nombre, 5);
+   ordenarPorNombre(nombre, sexos, 5);
 
-    printf("------------------\n");
+    printf("\n\n------------------\n");
     for(int i=0 ; i<5 ; i++){
-        puts(nombre[i]);
-        printf("\n");
+        printf("\n%s, %c", nombre[i], sexos[i]);
     }
+
+    ordenarPorNombreYSexo(nombre, sexos, 5);
+
+    printf("\n\n------------------\n");
+    for(int i=0 ; i<5 ; i++){
+        printf("\n%s, %c", nombre[i], sexos[i]);
+    }
+
 
 
 
     return 0;
 }
 
-void ordenarPorNombre (char nombre[][20], int tam){
+void ordenarPorNombre (char nombre[][20], char sexos[], int tam){
 
     //Ordena de manera creciente
     char auxString[10][20];
+    char auxChar;
 
     for(int i=0 ; i<tam-1 ; i++){
 
@@ -46,7 +56,37 @@ void ordenarPorNombre (char nombre[][20], int tam){
                 strcpy(nombre[i], nombre[j]);
                 strcpy(nombre[j], auxString[0]);
 
+                auxChar=sexos[i];
+                sexos[i]=sexos[j];
+                sexos[j]=auxChar;
+
                }
         }
     }
 }
+
+void ordenarPorNombreYSexo (char nombre[][20], char sexos[], int tam){
+
+    //Ordena de manera creciente
+    char auxString[10][20];
+    char auxChar;
+
+    for(int i=0 ; i<tam-1 ; i++){
+        for(int j=i+1 ; j<tam ; j++){
+
+            if(sexos[i]==sexos[j]){
+                if(stricmp(nombre[i], nombre[j])==1){
+                strcpy(auxString[0], nombre[i]);
+                strcpy(nombre[i], nombre[j]);
+                strcpy(nombre[j], auxString[0]);
+
+                auxChar=sexos[i];
+                sexos[i]=sexos[j];
+                sexos[j]=auxChar;
+               }
+            }
+        }
+    }
+}
+
+
